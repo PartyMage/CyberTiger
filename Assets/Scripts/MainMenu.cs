39e50b461dc87d8bfd97c08786baa39dc9a57df3
PartyMage <bhamil26@outlook.com>
2017-07-28 00:12:37 -0400

    making the main menu work

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,27 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+    public Image startScreen;
+    public Image creditsPage;
+    public int count;
+    public bool ableToClick;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ableToClick = false;
+        startScreen.gameObject.SetActive(true);
+        creditsPage.gameObject.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        count++;
+		if (ableToClick == true) {
+            if (Input.GetMouseButtonDown(0)) {
+                startScreen.gameObject.SetActive(false);
+                BackToMenu();
+            }
+
+
+        } else if (ableToClick == false){
+            if (count > 50) {
+                ableToClick = true;
+                
+            }
+        }
 	}
 
     //click to start the game
-    void TigerRun() {
+    public void TigerRun() {
         SceneManager.LoadScene(1);
-
     }
 
     //click to view credits
-    void CreditsClick() {
+    public void CreditsClick() {
+        creditsPage.gameObject.SetActive(true);
 
+        if (Input.GetMouseButtonDown(0)) {
+            BackToMenu();
+        }
+    }
+
+    public void BackToMenu() {
+        creditsPage.gameObject.SetActive(false);
     }
 }
