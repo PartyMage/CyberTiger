563c36bb05a92f58b07c9faad6e7187c9dc458e6
Party_Mage <bhamil26@outlook.com>
2017-09-02 17:27:44 -0400

    weapon selector work

---
+++ Assets/WeaponSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelector : MonoBehaviour {
+
+    public Image[] weaponSpots;
+    public Sprite[] weaponOptions;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < weaponOptions.Length; i++)
+        {
+            weaponSpots[i].sprite = weaponOptions[i];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
