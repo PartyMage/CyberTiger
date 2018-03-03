8ac4bf7e0e7c33919d5fc65002071fb46c0f8df0
Party_Mage <bhamil26@outlook.com>
2018-03-03 08:14:26 -0500

    story mode work

--- Assets/Scripts/WeaponSelector.cs
+++ Assets/Scripts/WeaponSelector.cs
@@ -23,7 +23,7 @@ public class WeaponSelector : MonoBehaviour {
 		
 	}
 
-    void openButtonPress() {
+    public void openButtonPress() {
         
     }
 }
---
+++ Assets/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    public Image jumpImage;
+    public Image fireImage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+    public void JumpClick() {
+        jumpImage.gameObject.SetActive(false);
+    }
+
+    public void FireClick() {
+        fireImage.gameObject.SetActive(false);
+
+    }
+}
